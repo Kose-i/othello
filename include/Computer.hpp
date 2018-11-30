3ff7278330598c7516fb8446ef414c75109aec4c
Kose-i <tamura.kosei905@mail.kyutech.jp>
2018-12-01 00:02:51 +0900

    Apply Hyouka class on Computer class

--- include/Computer.hpp
+++ include/Computer.hpp
@@ -3,27 +3,16 @@
 
 #include "Board.hpp"
 #include "Hand.hpp"
-//#include "Hyoukakansuu.hpp"
+#include "Hyoukakansuu.hpp"
 #include <vector>
 #include <memory>
 
-class Computer: public Hand// , Hyoukakansuu
+class Computer: public Hand ,private Hyoukakansuu
 {
   private:
     std::shared_ptr<Board> board_state;
     Board_env my_stone_color;
     Board_env enemy_stone_color;
-
-    std::vector<std::vector<int>> costmap{
-          {100,-40,20, 5, 5,20,-40,100},
-          {-40,-80,-1,-1,-1,-1,-80,-40},
-          { 20, -1, 5, 1, 1, 5, -1, 20},
-          {  5, -1, 1, 0, 0, 1, -1,  5},
-          {  5, -1, 1, 0, 0, 1, -1,  5},
-          { 20, -1, 5, 1, 1, 5, -1, 20},
-          {-40,-80,-1,-1,-1,-1,-80,-40},
-          {100,-40,20, 5, 5,20,-40,100},
-        };
 
   private://function
     void init_enemy_color();
