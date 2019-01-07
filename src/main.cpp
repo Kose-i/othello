6c6feb84861d25cc331319c1f91dda91c6a258ef
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-01-07 15:02:46 +0900

    Modify error code

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char** argv)
     game_.run();
     game_.result();
   } catch(...) {
-    std::cout << "error happen\n";
+    std::cerr << "error happen\n";
     return -1;
   }
   return 0;
