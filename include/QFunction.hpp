436351c5c8be6d4bd8c64fe7f252a1af2c2ed340
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-03-15 16:48:23 +0900

    Add QFunction class

---
+++ include/QFunction.hpp
@@ -0,0 +1,11 @@
+#ifndef QFunction_hpp
+#define QFunction_hpp
+
+#include <string>
+
+class QFunction{
+  public:
+    QFunction();
+    void set_file(const std::string&);
+}
+#endif
---
+++ src/QFunction.cpp
@@ -0,0 +1,6 @@
+#include "QFunction.hpp"
+
+#include <string>
+
+QFunction::QFunction(){}
+void QFunction::set_file(const std::string& file_name){}
